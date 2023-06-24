d339d2d87b97e1cc36cdb4722eb9b437b9a010dc
Ejay02 <e.jae02@gmail.com>
2023-06-24 14:20:54 +0100

    refactor code

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map, mapEvent;
 
 class App {
   #map;
@@ -19,14 +18,21 @@ class App {
 
   constructor() {
     this._getPositon();
+
+    form.addEventListener('submit', this._newWorkout.bind(this));
+
+    inputType.addEventListener('change', this._toggleElevationField);
   }
 
   _getPositon() {
     // GEOLOCATION API
     if (navigator.geolocation)
-      navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), function () {
-        alert("Couldn't get your location");
-      });
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert("Couldn't get your location");
+        }
+      );
   }
 
   _loadMap(position) {
@@ -36,8 +42,6 @@ class App {
 
     const coords = [latitude, longitude];
 
-    console.log(this);
-
     this.#map = L.map('map').setView(coords, 13);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -46,21 +50,45 @@ class App {
     }).addTo(this.#map);
 
     // handle clicks on map
-    this.#map.on('click', function (mapE) {
-     this.#mapEvent = mapE;
-      form.classList.remove('hidden');
-      inputDistance.focus();
-    });
+    this.#map.on('click', this._showForm.bind(this));
   }
 
-  _showForm() {}
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
 
-  _toggleElevationField() {}
+  _toggleElevationField() {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
 
-  _newWorkout() {}
+  _newWorkout(e) {
+    e.preventDefault();
+
+    //clear input fields
+    inputDistance.value =
+      inputCadence.value =
+      inputDuration.value =
+      inputElevation.value =
+        '';
+    // Display marker
+    const { lat, lng } = this.#mapEvent.latlng;
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup',
+        })
+      )
+      .setPopupContent('Coding 🤷🏽😑')
+      .openPopup();
+  }
 }
 
 const app = new App();
-
-
-
